8e25e0c9e02b094598d4728c6104a2024f49e76f
Peter Sebestyen <peti.sebes@outlook.com>
2024-04-09 05:15:51 +0200

    Added header

---
+++ src/Components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+        <div>
+            <h1>Ja<span className='violet'>mmm</span>ing</h1>
+        </div>
+    );
+};
+
+export default Header;
