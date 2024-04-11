7c440aa96d31e39d3ad2f87cfa405a6833020726
Peter Sebestyen <peti.sebes@outlook.com>
2024-04-11 05:08:50 +0200

    Added searchbutton and styled search

--- src/Components/searchBar/searchBar.js
+++ src/Components/searchBar/searchBar.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './searchBar.css';
 
 function SearchBar() {
+
+
+
     return (
-        <div className=''>
-            <input type='text' />
+        <div className='searchBar'>
+            <input placeholder='Enter a Song or Artist'/>
+            <button>Search</button>
         </div>
     )
 };
